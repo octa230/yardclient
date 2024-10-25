1c031d49b3d195d78e070ab818a50055a45c94ae
octa230 <mervynstunner@outlook.com>
2024-10-25 11:37:57 +0400

    home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -111,7 +111,7 @@ function HomeScreen() {
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <div className="featured">
-            {featuredProducts && featuredProducts.map((product) => (
+            {Array.isArray(featuredProducts) && featuredProducts.map((product) => (
               <Col key={product.slug} className="p-1 m-1 border"xs={4} md={4} lg={3} style={{width: "300px"}}>
                 <Link to={`/product/${product.slug}`}>
                 <Card.Img src={product.image} alt={product.name} 
@@ -126,7 +126,7 @@ function HomeScreen() {
       <LoadingBox />
     ) : error ? (
       <MessageBox variant='danger'>{error}</MessageBox>
-    ) : ( offers && offers.map((offer)=>(
+    ) : ( Array.isArray(offers) && offers.map((offer)=>(
           <OfferTemplateone key={offer._id} offer={offer}/>
         ))
     )}
@@ -138,7 +138,7 @@ function HomeScreen() {
         <MessageBox>No Stores</MessageBox>
       ): (
         <Row className="justify-content-center">
-          {shops && shops.map((shop)=> (
+          {Array.isArray(shops) && shops.map((shop)=> (
             <Col xs={12} md={3}key={shop._id}>
             <StoreCard shop={shop}/>
             </Col>
